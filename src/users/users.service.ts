0c200d1b112effe4fb8dc9fde8808a281f3246e6
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2022-08-07 16:38:55 -0300

    feat: connection with prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    const { name, email, isActive } = createUserDto;
+    return this.prisma.user.create({
+      data: {
+        name,
+        email,
+        isActive,
+      },
+    });
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
   }
 
   findAll() {
