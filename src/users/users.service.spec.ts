f73394dba871ff35fc91979f845d4aeb64324844
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2022-08-07 21:41:00 -0300

    test: add findAll test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,31 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Console } from 'console';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
-
-  const prismaServiceMock = {
-    user: {
-      update: jest.fn()
-    }
+  let prisma: PrismaService;
+  
+  const userMockedData: CreateUserDto = {
+    email: "example@email.com"
   }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UsersService,
-        { 
-          provide: PrismaService, 
-          useValue: prismaServiceMock 
-        }
-      ],
+      providers: [UsersService, PrismaService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+
+    it('should return an array of users', async() => {
+
+      prisma.user.findMany = jest.fn().mockReturnValue([userMockedData]);
+
+      const result = await service.findAll();
+
+      expect(result).toEqual([userMockedData]);
+      expect(prisma.user.findMany).toHaveBeenCalledTimes(1);
+      expect(prisma.user.findMany).toHaveBeenCalledWith({"where": {"isActive": true}});
+    })
+
+  })
 });
